4839fdc9f82f2a8f6def8323689b09442aabc824
yburdin <34156366+yburdin@users.noreply.github.com>
2022-08-02 14:41:04 +0300

    reorder

--- tests/test_all.py
+++ tests/test_all.py
@@ -7,10 +7,44 @@ from python.problems_2001to2500 import *
 
 
 class Test0001to0500(unittest.TestCase):
+    def test_028(self):
+        self.assertEqual(str_str(haystack="hello", needle="ll"), 2)
+        self.assertEqual(str_str(haystack="aaaaa", needle="bba"), -1)
+        self.assertEqual(str_str(haystack="a", needle="a"), 0)
+
+    def test_043(self):
+        self.assertEqual(multiply('2', '3'), '6')
+        self.assertEqual(multiply('123', '456'), '56088')
+
+    def test_058(self):
+        self.assertEqual(length_of_last_word("Hello World"), 5)
+        self.assertEqual(length_of_last_word("   fly me   to   the moon  "), 4)
+        self.assertEqual(length_of_last_word("luffy is still joyboy"), 6)
+
+    def test_066(self):
+        self.assertEqual(plus_one([1, 2, 3]), [1, 2, 4])
+        self.assertEqual(plus_one([4, 3, 2, 1]), [4, 3, 2, 2])
+        self.assertEqual(plus_one([9]), [1, 0])
+
+    def test_067(self):
+        self.assertEqual(add_binary(a="11", b="1"), "100")
+        self.assertEqual(add_binary(a="1010", b="1011"), "10101")
+
+    def test_070(self):
+        self.assertEqual(climb_stairs(2), 2)
+        self.assertEqual(climb_stairs(3), 3)
+        self.assertEqual(climb_stairs(4), 5)
+        self.assertEqual(climb_stairs(5), 8)
+
     def test_119(self):
         self.assertEqual(get_row(3), [1, 3, 3, 1])
         self.assertEqual(get_row(0), [1])
         self.assertEqual(get_row(1), [1, 1])
+
+    def test_150(self):
+        self.assertEqual(eval_rpn(["2", "1", "+", "3", "*"]), 9)
+        self.assertEqual(eval_rpn(["4", "13", "5", "/", "+"]), 6)
+        self.assertEqual(eval_rpn(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]), 22)
 
     def test_191(self):
         self.assertEqual(hamming_weight(0b00000000000000000000000000001011), 3)
@@ -21,29 +55,10 @@ class Test0001to0500(unittest.TestCase):
         self.assertEqual(is_happy(19), True)
         self.assertEqual(is_happy(2), False)
 
-    def test_389(self):
-        self.assertEqual(find_the_difference(s="abcd", t="abcde"), 'e')
-        self.assertEqual(find_the_difference(s="", t="y"), 'y')
-        self.assertEqual(find_the_difference(s="a", t="aa"), 'a')
-
-    def test_405(self):
-        self.assertEqual(to_hex(26), '1a')
-        self.assertEqual(to_hex(-1), 'ffffffff')
-
-    def test_496(self):
-        self.assertEqual(next_greater_element([4, 1, 2], [1, 3, 4, 2]), [-1, 3, -1])
-        self.assertEqual(next_greater_element([2, 4], [1, 2, 3, 4]), [3, -1])
-        
-    def test_070(self):
-        self.assertEqual(climb_stairs(2), 2)
-        self.assertEqual(climb_stairs(3), 3)
-        self.assertEqual(climb_stairs(4), 5)
-        self.assertEqual(climb_stairs(5), 8)
-
-    def test_028(self):
-        self.assertEqual(str_str(haystack="hello", needle="ll"), 2)
-        self.assertEqual(str_str(haystack="aaaaa", needle="bba"), -1)
-        self.assertEqual(str_str(haystack="a", needle="a"), 0)
+    def test_217(self):
+        self.assertTrue(contains_duplicate([1, 2, 3, 1]))
+        self.assertFalse(contains_duplicate([1, 2, 3, 4]))
+        self.assertTrue(contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
 
     def test_232(self):
         my_queue = MyQueue()
@@ -58,16 +73,20 @@ class Test0001to0500(unittest.TestCase):
         self.assertFalse(is_anagram(s="rat", t="car"))
         self.assertFalse(is_anagram(s="ac", t="bb"))
 
-    def test_217(self):
-        self.assertTrue(contains_duplicate([1, 2, 3, 1]))
-        self.assertFalse(contains_duplicate([1, 2, 3, 4]))
-        self.assertTrue(contains_duplicate([1,1,1,3,3,4,3,2,4,2]))
-
     def test_303(self):
         num_array = NumArray([-2, 0, 3, -5, 2, -1])
         self.assertEqual(num_array.sum_range(0, 2), 1)
         self.assertEqual(num_array.sum_range(2, 5), -1)
         self.assertEqual(num_array.sum_range(0, 5), -3)
+
+    def test_389(self):
+        self.assertEqual(find_the_difference(s="abcd", t="abcde"), 'e')
+        self.assertEqual(find_the_difference(s="", t="y"), 'y')
+        self.assertEqual(find_the_difference(s="a", t="aa"), 'a')
+
+    def test_405(self):
+        self.assertEqual(to_hex(26), '1a')
+        self.assertEqual(to_hex(-1), 'ffffffff')
 
     def test_459(self):
         self.assertTrue(repeated_substring_pattern("abab"))
@@ -75,28 +94,9 @@ class Test0001to0500(unittest.TestCase):
         self.assertTrue(repeated_substring_pattern("abcabcabcabc"))
         self.assertFalse(repeated_substring_pattern("aabaaba"))
 
-    def test_066(self):
-        self.assertEqual(plus_one([1, 2, 3]), [1, 2, 4])
-        self.assertEqual(plus_one([4, 3, 2, 1]), [4, 3, 2, 2])
-        self.assertEqual(plus_one([9]), [1, 0])
-
-    def test_150(self):
-        self.assertEqual(eval_rpn(["2", "1", "+", "3", "*"]), 9)
-        self.assertEqual(eval_rpn(["4", "13", "5", "/", "+"]), 6)
-        self.assertEqual(eval_rpn(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]), 22)
-
-    def test_043(self):
-        self.assertEqual(multiply('2', '3'), '6')
-        self.assertEqual(multiply('123', '456'), '56088')
-
-    def test_067(self):
-        self.assertEqual(add_binary(a="11", b="1"), "100")
-        self.assertEqual(add_binary(a="1010", b="1011"), "10101")
-
-    def test_058(self):
-        self.assertEqual(length_of_last_word("Hello World"), 5)
-        self.assertEqual(length_of_last_word("   fly me   to   the moon  "), 4)
-        self.assertEqual(length_of_last_word("luffy is still joyboy"), 6)
+    def test_496(self):
+        self.assertEqual(next_greater_element([4, 1, 2], [1, 3, 4, 2]), [-1, 3, -1])
+        self.assertEqual(next_greater_element([2, 4], [1, 2, 3, 4]), [3, -1])
 
 
 class Test0501to1000(unittest.TestCase):
@@ -105,11 +105,27 @@ class Test0501to1000(unittest.TestCase):
         self.assertEqual(to_lower_case("here"), "here")
         self.assertEqual(to_lower_case("LOVELY"), "lovely")
 
+    def test_739(self):
+        self.assertEqual(daily_temperatures(temperatures=[73, 74, 75, 71, 69, 72, 76, 73]), [1, 1, 4, 2, 1, 1, 0, 0])
+        self.assertEqual(daily_temperatures(temperatures=[30, 40, 50, 60]), [1, 1, 1, 0])
+        self.assertEqual(daily_temperatures(temperatures=[30, 60, 90]), [1, 1, 0])
+        self.assertEqual(daily_temperatures(temperatures=[89, 62, 70, 58, 47, 47, 46, 76, 100, 70]),
+                         [8, 1, 5, 4, 3, 2, 1, 1, 0, 0])
+
     def test_797(self):
         self.assertCountEqual(all_paths_source_target([[1, 2], [3], [3], []]),
                               [[0, 1, 3], [0, 2, 3]])
         self.assertCountEqual(all_paths_source_target([[4, 3, 1], [3, 2, 4], [3], [4], []]),
                               [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]])
+
+    def test_876(self):
+        self.assertEqual(linked_list_to_list(middle_node(list_to_linked_list([1, 2, 3, 4, 5]))), [3, 4, 5])
+        self.assertEqual(linked_list_to_list(middle_node(list_to_linked_list([1, 2, 3, 4, 5, 6]))), [4, 5, 6])
+
+    def test_896(self):
+        self.assertTrue(is_monotonic([1, 2, 2, 3]))
+        self.assertTrue(is_monotonic([6, 5, 4, 4]))
+        self.assertFalse(is_monotonic([1, 3, 2]))
 
     def test_953(self):
         self.assertTrue(is_alien_sorted(words=["hello", "leetcode"], order="hlabcdefgijkmnopqrstuvwxyz"))
@@ -121,28 +137,12 @@ class Test0501to1000(unittest.TestCase):
         self.assertEqual(largest_perimeter([1, 2, 1]), 0)
         self.assertEqual(largest_perimeter([2, 6, 2, 5, 4, 15, 1]), 15)
 
-    def test_876(self):
-        self.assertEqual(linked_list_to_list(middle_node(list_to_linked_list([1, 2, 3, 4, 5]))), [3, 4, 5])
-        self.assertEqual(linked_list_to_list(middle_node(list_to_linked_list([1, 2, 3, 4, 5, 6]))), [4, 5, 6])
-
-    def test_896(self):
-        self.assertTrue(is_monotonic([1, 2, 2, 3]))
-        self.assertTrue(is_monotonic([6, 5, 4, 4]))
-        self.assertFalse(is_monotonic([1, 3, 2]))
-
     def test_989(self):
         self.assertEqual(add_to_array_form(num=[1, 2, 0, 0], k=34), [1, 2, 3, 4])
         self.assertEqual(add_to_array_form(num=[2, 7, 4], k=181), [4, 5, 5])
         self.assertEqual(add_to_array_form(num=[2, 1, 5], k=806), [1, 0, 2, 1])
         self.assertEqual(add_to_array_form(num=[0], k=23), [2, 3])
         self.assertEqual(add_to_array_form(num=[0], k=10000), [1, 0, 0, 0, 0])
-
-    def test_739(self):
-        self.assertEqual(daily_temperatures(temperatures=[73, 74, 75, 71, 69, 72, 76, 73]), [1, 1, 4, 2, 1, 1, 0, 0])
-        self.assertEqual(daily_temperatures(temperatures=[30, 40, 50, 60]), [1, 1, 1, 0])
-        self.assertEqual(daily_temperatures(temperatures=[30, 60, 90]), [1, 1, 0])
-        self.assertEqual(daily_temperatures(temperatures=[89, 62, 70, 58, 47, 47, 46, 76, 100, 70]),
-                         [8, 1, 5, 4, 3, 2, 1, 1, 0, 0])
 
 
 class Test1001to1500(unittest.TestCase):
@@ -158,9 +158,19 @@ class Test1001to1500(unittest.TestCase):
         self.assertEqual(subtract_product_and_sum(234), 15)
         self.assertEqual(subtract_product_and_sum(4421), 21)
 
+    def test_1290(self):
+        self.assertEqual(get_decimal_value(list_to_linked_list([1, 0, 1])), 5)
+        self.assertEqual(get_decimal_value(list_to_linked_list([0])), 0)
+        self.assertEqual(get_decimal_value(list_to_linked_list([1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0])), 18880)
+
     def test_1309(self):
         self.assertEqual(freq_alphabets("10#11#12"), "jkab")
         self.assertEqual(freq_alphabets("1326#"), "acz")
+
+    def test_1356(self):
+        self.assertEqual(sort_by_bits([0, 1, 2, 3, 4, 5, 6, 7, 8]), [0, 1, 2, 4, 8, 3, 5, 6, 7])
+        self.assertEqual(sort_by_bits([1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]),
+                         [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024])
 
     def test_1437(self):
         self.assertEqual(k_length_apart([1, 0, 0, 0, 1, 0, 0, 1], 2), True)
@@ -170,16 +180,6 @@ class Test1001to1500(unittest.TestCase):
     def test_1491(self):
         self.assertEqual(average([4000, 3000, 1000, 2000]), 2500.)
         self.assertEqual(average([1000, 2000, 3000]), 2000.)
-
-    def test_1290(self):
-        self.assertEqual(get_decimal_value(list_to_linked_list([1, 0, 1])), 5)
-        self.assertEqual(get_decimal_value(list_to_linked_list([0])), 0)
-        self.assertEqual(get_decimal_value(list_to_linked_list([1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0])), 18880)
-
-    def test_1356(self):
-        self.assertEqual(sort_by_bits([0, 1, 2, 3, 4, 5, 6, 7, 8]), [0, 1, 2, 4, 8, 3, 5, 6, 7])
-        self.assertEqual(sort_by_bits([1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]),
-                         [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024])
 
 
 class Test1501to2000(unittest.TestCase):
@@ -204,6 +204,13 @@ class Test1501to2000(unittest.TestCase):
         self.assertEqual(sum_odd_length_subarrays([1, 4, 2, 5, 3]), 58)
         self.assertEqual(sum_odd_length_subarrays([1, 2]), 3)
         self.assertEqual(sum_odd_length_subarrays([10, 11, 12]), 66)
+
+    def test_1603(self):
+        parking_system = ParkingSystem(1, 1, 0)
+        self.assertTrue(parking_system.add_car(1))
+        self.assertTrue(parking_system.add_car(2))
+        self.assertFalse(parking_system.add_car(3))
+        self.assertFalse(parking_system.add_car(1))
 
     def test_1672(self):
         self.assertEqual(maximum_wealth([[1, 2, 3], [3, 2, 1]]), 6)
@@ -252,13 +259,6 @@ class Test1501to2000(unittest.TestCase):
     def test_1925(self):
         self.assertEqual(count_triples(5), 2)
         self.assertEqual(count_triples(10), 4)
-
-    def test_1603(self):
-        parking_system = ParkingSystem(1, 1, 0)
-        self.assertTrue(parking_system.add_car(1))
-        self.assertTrue(parking_system.add_car(2))
-        self.assertFalse(parking_system.add_car(3))
-        self.assertFalse(parking_system.add_car(1))
 
 
 class Test2001to2500(unittest.TestCase):
