a6c06f68387ef5f72b7c2cb1390d136bc8d76564
YBurdin <yuun@gmx.com>
2023-02-05 12:11:45 +0300

    355

--- tests/test_all.py
+++ tests/test_all.py
@@ -134,6 +134,33 @@ class Test0001to0500(unittest.TestCase):
         self.assertEqual(num_array.sum_range(2, 5), -1)
         self.assertEqual(num_array.sum_range(0, 5), -3)
 
+    def test_355(self):
+        with self.subTest('Example 1'):
+            twitter = Twitter()
+
+            # User 1 posts a new tweet (id = 5).
+            twitter.postTweet(1, 5)
+
+            # User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+            twitter.getNewsFeed(1)
+
+            # User 1 follows user 2.
+            twitter.follow(1, 2)
+
+            # User 2 posts a new tweet (id = 6).
+            twitter.postTweet(2, 6)
+
+            # User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+            # Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+            self.assertEqual(twitter.getNewsFeed(1), [6, 5])
+
+            # User 1 unfollows user 2.
+            twitter.unfollow(1, 2)
+
+            # User 1's news feed should return a list with 1 tweet id -> [5],
+            # since user 1 is no longer following user 2.
+            self.assertEqual(twitter.getNewsFeed(1), [5])
+
     def test_389(self):
         self.assertEqual(find_the_difference(s="abcd", t="abcde"), 'e')
         self.assertEqual(find_the_difference(s="", t="y"), 'y')
